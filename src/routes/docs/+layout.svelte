a1b292e342569ad047e8290bd4494500a6d6e41e
Hugo <hugokorteapple@gmail.com>
2023-06-15 12:11:07 +0200

    added better padding to docs layout

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -4,6 +4,6 @@
 
 <section class="lg:grid lg:grid-cols-[250px_1fr_250px]">
     <aside class="hidden lg:block sticky top-[var(--header-height)] h-[calc(100vh-var(--header-height))]"><DocsSidebar /></aside>
-    <div class="p-4"><slot /></div>
+    <div class="py-16 px-[10vw]"><slot /></div>
     <aside class="hidden lg:block sticky top-[var(--header-height)] h-[calc(100vh-var(--header-height))]"><slot name="sidebarRight" /></aside>
 </section>
